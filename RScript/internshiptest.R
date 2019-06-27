01644f19230b5a990b9b66fcba9a5d428ab9e10b
Domitille COQ--ETCHEGARAY <ayeshafarron@gmail.com>
2019-06-27 15:58:25 +0200

    Maj

--- RScript/internshiptest.R
+++ RScript/internshiptest.R
@@ -5,10 +5,9 @@ library(UpSetR)
 
 ####MISSMDA
 #Couverture
-coverage <- read.csv("test_multi_data_coverage2.csv",header=TRUE,sep="\t")
+coverage <- read.csv("./OutputData/test_multi_data_coverage.csv",header=TRUE,sep="\t")
 row.names(coverage) <- coverage$POS
 coverage <- coverage[,-1]
-hist(na.omit(coverage$bwasam),xlim=c(0,10),breaks=500)
 summary(coverage)
 #Gestion des NA
 comp <- imputePCA(coverage,ncp=2,scale=TRUE)
@@ -16,17 +15,17 @@ res.pca <- PCA(comp$completeObs)
 #Sans gestion
 res.pca <- PCA(coverage,scale.unit = TRUE,ncp=2,graph=T)
 
-#AllFreq
-allfreq <- read.csv("test_multi_data_allfreq.csv",header=TRUE,sep="\t")
-summary(allfreq)
-row.names(allfreq) <- allfreq$POS
-allfreq <- allfreq[,-1]
-normalize(allfreq, method = "standardize", range = c(0, 1), margin = 1L, on.constant = "quiet")
-#Gestion des NA
-comp2 <- imputePCA(allfreq,ncp=2,scale=FALSE)
-res.pca2 <- PCA(comp2$completeObs)
-#Sans gestion
-res.pca2 <- PCA(allfreq,scale.unit = TRUE,ncp=2,graph=T)
+# #AllFreq
+# allfreq <- read.csv("test_multi_data_allfreq.csv",header=TRUE,sep="\t")
+# summary(allfreq)
+# row.names(allfreq) <- allfreq$POS
+# allfreq <- allfreq[,-1]
+# normalize(allfreq, method = "standardize", range = c(0, 1), margin = 1L, on.constant = "quiet")
+# #Gestion des NA
+# comp2 <- imputePCA(allfreq,ncp=2,scale=FALSE)
+# res.pca2 <- PCA(comp2$completeObs)
+# #Sans gestion
+# res.pca2 <- PCA(allfreq,scale.unit = TRUE,ncp=2,graph=T)
 
 
 ####UPSETR
@@ -35,3 +34,4 @@ row.names(PA) <- as.numeric(PA$POS)
 PA <- PA[,-1]
 summary(PA)
 upset(PA,nsets=10,order.by="freq")
+  
